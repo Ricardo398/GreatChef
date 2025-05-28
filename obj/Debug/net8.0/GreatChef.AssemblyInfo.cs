4ffcadf4ddaa5dca805ca6a2b4cf2f0760baaa2d
Ricardo Augusto <ricxd_@hotmail.com>
2025-05-28 01:07:31 -0300

    feat: Improving the home page
    Now was created the first home page version with interactive cards and the others visual components.

--- obj/Debug/net8.0/GreatChef.AssemblyInfo.cs
+++ obj/Debug/net8.0/GreatChef.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreatChef")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba43d86114c142ed4301caa6869202d596c1823d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b226c6bc9b2072e5fa068332480ddd3f9b8d80d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreatChef")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreatChef")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
